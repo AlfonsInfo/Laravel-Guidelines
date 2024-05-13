2de77f601d1dbe72c5b6863061e8430a658c99d0
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2024-05-13 23:31:13 +0700

    Feat : Log Query Provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,18 +2,22 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
-        //
+        if ($this->app->environment('local')) {
+            //* Log All Queries
+            DB::listen(function(QueryExecuted $query){
+                // Log::info($query->sql);
+            });
+            // $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            // $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 
     /**
